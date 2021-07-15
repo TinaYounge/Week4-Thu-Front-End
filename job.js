8d96b296b7b18b4b15396cc6ff6a11afe9e3ca8b
Tina <nguyennga.hcm@gmail.com>
2021-07-15 16:08:44 +0700

    add form but have not work

--- job.js
+++ job.js
@@ -1,29 +1,29 @@
-let url = `http://localhost:5000/companies`;
+let url = `http://localhost:5000/jobs`;
 
-async function getCompanies() {
+async function getJobs() {
   try {
     const response = await fetch(url);
     const json = await response.json();
-    const companies = json;
-    console.log("companies", companies);
-    const companiesHTML = [];
-    for (const company of companies) {
-      companiesHTML.push(renderCompanies(company));
+    const jobs = json;
+    console.log("jobs", jobs);
+    const jobsHTML = [];
+    for (const job of jobs) {
+      jobsHTML.push(renderjobs(job));
     }
-    document.getElementById("companiesCard").innerHTML = companiesHTML.join("");
+    document.getElementById("companyJobs").innerHTML = jobsHTML.join("");
   } catch (error) {
     console.log(error);
-    companies = JSON.parse(localStorage.getItem("Some thing is not good"));
+    jobs = JSON.parse(localStorage.getItem("Some thing is not good"));
   }
 }
 
-function renderCompanies(company) {
+function renderjobs(job) {
   return `
   <div class="col-md-4" id= "">
   <div class="card mb-4 shadow-sm">
     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
     <div class="card-body">
-      <p class="card-text">${company.name}</p>
+      <p class="card-text">${job.title}</p>
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
           <button type="button" class="btn btn-sm btn-outline-secondary" ><a href  ="./jobsinCom.html ">View</a></button>
@@ -36,4 +36,14 @@ function renderCompanies(company) {
 </div>
   `;
 }
-getCompanies();
+getJobs();
+
+const form = document.getElementById("jobCreate");
+console.log("form", form);
+form.addEventListener("submit", (el) => {
+  console.log("el", { el });
+  el.preventDefault();
+  const title = document.getElementById("title").value;
+  const city = document.getElementById("city").value;
+  console.log("city,vale", { city, title });
+});
