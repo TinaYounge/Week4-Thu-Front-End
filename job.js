6f1d4ac53b5a8cb7a222b08b977e3ecfdf311890
Tina <nguyennga.hcm@gmail.com>
2021-07-15 21:46:23 +0700

    Job card included title and city

--- job.js
+++ job.js
@@ -23,11 +23,13 @@ function renderjobs(job) {
   <div class="card mb-4 shadow-sm">
     <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
     <div class="card-body">
-      <p class="card-text">${job.title}</p>
+      <p class="card-text p-thick">${job.title}</p>
+      <p class="card-text ">${job.city}</p>
+
       <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
           <button type="button" class="btn btn-sm btn-outline-secondary" ><a href  ="./jobsinCom.html ">View</a></button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Save</button>
         </div>
         <small class="text-muted">9 mins</small>
       </div>
@@ -41,9 +43,8 @@ getJobs();
 const form = document.getElementById("jobCreate");
 console.log("form", form);
 form.addEventListener("submit", (el) => {
-  console.log("hahah");
   console.log("el", { el });
-  el.preventDefault();
+  // el.preventDefault();
   const title = form.elements["title"].value;
   const city = form.elements["city"].value;
   console.log("city,vale", { city, title });
